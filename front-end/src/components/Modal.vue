ae1f98d79db915d23c9c8d6710a218464f1e295e
Pedro Augusto <pedroaugustoa47@gmail.com>
2025-07-21 17:32:26 -0300

    style: Style adjustment on Modal abstract component

--- front-end/src/components/Modal.vue
+++ front-end/src/components/Modal.vue
@@ -10,7 +10,7 @@ const props = defineProps({ show: Boolean });
 			@click.self="$emit('close')"
 		>
 			<div
-				class="modal-container bg-gray-200 text-black w-xl min-h-20 rounded-md px-3 py-1 transition-transform duration-300 ease-linear"
+				class="modal-container bg-gray-200 text-black w-xl min-h-20 rounded-md px-3 py-2 transition-transform duration-300 ease-linear"
 			>
 				<slot name="body"></slot>
 				<slot name="button-area"></slot>
